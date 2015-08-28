492a25ba45f49ab4644727e25db397cd6b5e66e9
Jaclyn & Teresa Nesteby <pair+jkarnowski.teresanesteby@devbootcamp.com>
2015-08-27 19:54:36 -0700

    adds validations for users and organizations

--- chariteer/app/models/organization.rb
+++ chariteer/app/models/organization.rb
@@ -1,3 +1,8 @@
 class Organization < ActiveRecord::Base
+  has_secure_password
   has_many :events
+  validates: :contact_email, presence: true, uniqueness: true
+  validates: :name, presence: true
+
+# non-profit status?
 end
--- chariteer/app/models/user.rb
+++ chariteer/app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_many :events
 
   has_secure_password
+
+  validates: :email, presence: true, uniqueness: true
+  validates: :first_name, presence: true
+  validates: :last_name, presence: true
+  validates: :password, presence: true
 end
