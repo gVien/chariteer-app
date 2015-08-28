679b99b1fbec5a90e525631957e513d23a476b07
Jaclyn & Teresa Nesteby <pair+jkarnowski.teresanesteby@devbootcamp.com>
2015-08-27 19:33:04 -0700

    setup user methods CRUD

--- chariteer/app/controllers/users_controller.rb
+++ chariteer/app/controllers/users_controller.rb
@@ -5,9 +5,25 @@ class UsersController < ApplicationController
   def new
   end
 
-  def show
-  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:id] = @user.id
+      redirect_to #somewhere
+      # ie: redirect_to @user
+    else
+      flash[:error] = "email or password is wrong"
+      render 'new'
+    end
+end
 
-  def edit
+def show
+end
+
+def edit
+end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
