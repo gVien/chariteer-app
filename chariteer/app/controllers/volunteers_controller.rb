4abcd215cb9bb1168603fe4c4bde1820797bd69c
jkarnowski <jaclyn.karnowski@gmail.com>
2015-08-28 21:09:08 -0700

    add api routes and update model for api but breaking at autoload for volunteers

--- chariteer/app/controllers/volunteers_controller.rb
+++ chariteer/app/controllers/volunteers_controller.rb
@@ -1,29 +1,42 @@
-class VolunteersController < ApplicationController
-  include SessionsHelper
-  def index
-  end
+module Api
+  class VolunteersController < ApplicationController
+    respond_to :json
 
-  def new
-     @volunteer = Volunteer.new
-  end
+    include SessionsHelper
+    
+    def index
+      @volunteers = Volunteer.all
+    end
 
-  def show
-    @volunteer = Volunteer.new(volunteer_params)
-    if @volunteer.save
-      session[:id] = @volunteer.id
-      redirect_to #somewhere
-      # ie: redirect_to @volunteer
-    else
-      flash[:error] = "email or password is wrong"
-      render 'new'
+    def new
+       volunteer = Volunteer.new
+    end
+
+    def create
+      volunteer = Volunteer.new(volunteer_params)
+      if volunteer.save
+        session[:id] = volunteer.id
+        # redirect_to #somewhere
+        # ie: redirect_to @volunteer
+      else
+        flash[:error] = "email or password is wrong"
+        # render 'new'
+      end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    private
+    def volunteer_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
   end
 
-  def edit
-  end
-
-  private
-  def volunteer_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
-  end
 end
--- chariteer/config/routes.rb
+++ chariteer/config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
+  scope "/Api" do
+    # resources :sessions
+    resources :volunteers
 
+    resources :organizations do
+      resources :events
+    end
+    
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :sessions
-  resources :volunteers
-
-
-  resources :organizations do
-    resources :events
-  end
 
   # You can have the root of your site routed with "root"
-   root 'sessions#index'
+   # root 'sessions#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
